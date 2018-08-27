b46b544837bef597af25cd5285c631cc0887f5ae
Victor Rendon <victor.rs@eucledian.mx>
2018-08-27 14:10:27 -0500

    pyramid done

--- 3d_figures.js
+++ 3d_figures.js
@@ -1,6 +1,6 @@
 var projectionMatrix;
 
-var shaderProgram, shaderVertexPositionAttribute, shaderVertexColorAttribute, 
+var shaderProgram, shaderVertexPositionAttribute, shaderVertexColorAttribute,
     shaderProjectionMatrixUniform, shaderModelViewMatrixUniform;
 
 var duration = 5000; // ms
@@ -8,7 +8,7 @@ var duration = 5000; // ms
 // Attributes: Input variables used in the vertex shader. Since the vertex shader is called on each vertex, these will be different every time the vertex shader is invoked.
 // Uniforms: Input variables for both the vertex and fragment shaders. These do not change values from vertex to vertex.
 // Varyings: Used for passing data from the vertex shader to the fragment shader. Represent information for which the shader can output different value for each vertex.
-var vertexShaderSource =    
+var vertexShaderSource =
     "    attribute vec3 vertexPos;\n" +
     "    attribute vec4 vertexColor;\n" +
     "    uniform mat4 modelViewMatrix;\n" +
@@ -27,7 +27,7 @@ var vertexShaderSource =
 // - highp for vertex positions,
 // - mediump for texture coordinates,
 // - lowp for colors.
-var fragmentShaderSource = 
+var fragmentShaderSource =
     "    precision lowp float;\n" +
     "    varying vec4 vColor;\n" +
     "    void main(void) {\n" +
@@ -39,10 +39,10 @@ function initWebGL(canvas)
     var gl = null;
     var msg = "Your browser does not support WebGL, " +
         "or it is not enabled by default.";
-    try 
+    try
     {
         gl = canvas.getContext("experimental-webgl");
-    } 
+    }
     catch (e)
     {
         msg = "Error creating WebGL Context!: " + e.toString();
@@ -54,7 +54,7 @@ function initWebGL(canvas)
         throw new Error(msg);
     }
 
-    return gl;        
+    return gl;
  }
 
 function initViewport(gl, canvas)
@@ -62,14 +62,108 @@ function initViewport(gl, canvas)
     gl.viewport(0, 0, canvas.width, canvas.height);
 }
 
-function initGL(canvas)
-{
+function initGL(canvas) {
     // Create a project matrix with 45 degree field of view
     projectionMatrix = mat4.create();
-    mat4.perspective(projectionMatrix, Math.PI / 4, canvas.width / canvas.height, 1, 10000);
+    mat4.perspective(projectionMatrix, Math.PI / 32, canvas.width / canvas.height, 1, 10000);
+    mat4.translate(projectionMatrix, projectionMatrix, [0, 0, -40]);
 }
 
-// TO DO: Create the functions for each of the figures.
+function createPyramid(gl, translation, rotationAxis) {
+  // Vertex Data
+  var vertexBuffer;
+  vertexBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+
+  var v1 = [0.0, -1.5, -1.0];
+  var v2 = [-.95, -1.5, -.31];
+  var v3 = [-.59, -1.5, .81];
+  var v4 = [.59, -1.5, .81];
+  var v5 = [.95, -1.5, -.31];
+  var v6 = [0.0, 1.0, 0.0];
+
+  var verts = [
+     //Base
+     ...v1, ...v2, ...v3, ...v4, ...v5,
+
+     //Face 1
+     ...v1, ...v2, ...v6,
+
+     //Face 2
+     ...v2, ...v3, ...v6,
+
+     //Face 3
+     ...v3, ...v4, ...v6,
+     //Face 4
+     ...v4, ...v5, ...v6,
+
+     //Face 5
+     ...v5, ...v1, ...v6,
+     ];
+
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(verts), gl.STATIC_DRAW);
+
+  // Color data
+  var colorBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+  var faceColors = [
+      [1.0, 1.0, 0.0, 1.0],
+      [0.0, 1.0, 1.0, 1.0],
+      [1.0, 0.0, 0.0, 1.0],
+      [0.0, 1.0, 0.0, 1.0],
+      [1.0, 0.0, 1.0, 1.0],
+      [0.0, 0.0, 1.0, 1.0],
+  ];
+
+  var vertexColors = [];
+
+  // Assign color
+  var vertices = [5, 3, 3, 3, 3, 3]
+
+  var j = 0;
+  for (const color of faceColors) {
+    for (var k=0; k < vertices[j]; k++)
+      vertexColors = vertexColors.concat(color);
+    j++
+  }
+
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexColors), gl.STATIC_DRAW);
+
+  // Defines the triangles to be drawn
+  var pyramidIndexBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, pyramidIndexBuffer);
+  var pyramidIndices = [
+      0, 1, 2,    0, 2, 3,    0, 3, 4, //base
+      5, 6, 7,
+      8, 9, 10,
+      11, 12, 13,
+      14, 15, 16,
+      17, 18, 19
+  ];
+
+  // gl.ELEMENT_ARRAY_BUFFER: Buffer used for element indices.
+  // Uint16Array: Array of 16-bit unsigned integers.
+  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(pyramidIndices), gl.STATIC_DRAW);
+
+  var pyramid = {
+    buffer:vertexBuffer, colorBuffer:colorBuffer, indices:pyramidIndexBuffer,
+    vertSize:3, nVerts:20, colorSize:4, nColors: 20, nIndices:24,
+    primtype:gl.TRIANGLES, modelViewMatrix: mat4.create(), currentTime : Date.now()};
+  mat4.translate(pyramid.modelViewMatrix, pyramid.modelViewMatrix, translation);
+
+
+  pyramid.update = function() {
+    var now = Date.now();
+    var deltat = now - this.currentTime;
+    this.currentTime = now;
+    var fract = deltat / duration;
+    var angle = Math.PI * 2 * fract;
+    var up = false;
+    mat4.rotate(this.modelViewMatrix, this.modelViewMatrix, angle, rotationAxis);
+  };
+
+  return pyramid;
+}
 
 function createShader(gl, str, type)
 {
@@ -111,7 +205,7 @@ function initShader(gl)
 
     shaderVertexColorAttribute = gl.getAttribLocation(shaderProgram, "vertexColor");
     gl.enableVertexAttribArray(shaderVertexColorAttribute);
-    
+
     shaderProjectionMatrixUniform = gl.getUniformLocation(shaderProgram, "projectionMatrix");
     shaderModelViewMatrixUniform = gl.getUniformLocation(shaderProgram, "modelViewMatrix");
 
@@ -120,7 +214,7 @@ function initShader(gl)
     }
 }
 
-function draw(gl, objs) 
+function draw(gl, objs)
 {
     // clear the background (with black)
     gl.clearColor(0.1, 0.1, 0.1, 1.0);
@@ -140,7 +234,7 @@ function draw(gl, objs)
 
         gl.bindBuffer(gl.ARRAY_BUFFER, obj.colorBuffer);
         gl.vertexAttribPointer(shaderVertexColorAttribute, obj.colorSize, gl.FLOAT, false, 0, 0);
-        
+
         gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, obj.indices);
 
         gl.uniformMatrix4fv(shaderProjectionMatrixUniform, false, projectionMatrix);
@@ -156,7 +250,7 @@ function draw(gl, objs)
     }
 }
 
-function run(gl, objs) 
+function run(gl, objs)
 {
     // The window.requestAnimationFrame() method tells the browser that you wish to perform an animation and requests that the browser call a specified function to update an animation before the next repaint. The method takes a callback as an argument to be invoked before the repaint.
     requestAnimationFrame(function() { run(gl, objs); });
